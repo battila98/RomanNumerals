794724c1bda542849d94f1184ac3767524060adb
battila98 <55810364+battila98@users.noreply.github.com>
2020-11-17 20:02:15 +0100

    init

---
+++ RomanNumerals/RomanNumerals.Test/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("RomanNumerals")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("RomanNumerals")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e82be851-3668-4b00-8f36-be3d3a98135d")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ RomanNumerals/RomanNumerals/UnitTest1.cs
@@ -0,0 +1,15 @@
+﻿
+using NUnit.Framework;
+
+namespace RomanNumerals.Test
+{
+    class UnitTest
+    {
+        [Test]
+        public void ToRoman1()
+        {
+            Assert.Pass();
+        }
+    }
+
+}
